7cecedcef99d8cfb5c306052643fa0a925d42c8b
Hamuko <hamuko@burakku.com>
2015-09-17 14:10:09 +0300

    Fix output.list() crashing on empty lists.

--- cum/output.py
+++ cum/output.py
@@ -16,19 +16,20 @@ def error(msg):
 
 def list(items):
     """Print out a list of items in columns much like `ls` in bash."""
-    width = click.get_terminal_size()[0]
-    longest = len(max(items, key=len))
-    columns = int(width // (longest + 0.5))
-    rows = ceil(len(items) / columns)
+    if items:
+        width = click.get_terminal_size()[0]
+        longest = len(max(items, key=len))
+        columns = int(width // (longest + 0.5))
+        rows = ceil(len(items) / columns)
 
-    for row in range(rows):
-        line = []
-        for column in range(columns):
-            i = rows * column + row
-            if i >= len(items):
-                continue
-            line.append(items[i].ljust(longest))
-        click.echo(' '.join(line))
+        for row in range(rows):
+            line = []
+            for column in range(columns):
+                i = rows * column + row
+                if i >= len(items):
+                    continue
+                line.append(items[i].ljust(longest))
+            click.echo(' '.join(line))
 
 
 def series(msg):
