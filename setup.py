3c3e102061f56bae42d03f77b1dcd74f0a737b17
Hamuko <hamuko@burakku.com>
2015-10-02 17:47:45 +0300

    Bump the version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='cum',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     py_modules=['cum'],
     install_requires=[
