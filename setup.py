5c82bd2fd8d1f4043c51da7db7197ca89fa1ecdb
Hamuko <hamuko@burakku.com>
2016-04-27 19:00:19 +0300

    Use a setup version tag that conforms to PEP 440.

--- setup.py
+++ setup.py
@@ -1,7 +1,21 @@
 from setuptools import find_packages, setup
-import sys
 import os
+import re
 import subprocess
+import sys
+
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+git_path = os.path.join(script_path, '.git')
+re_version = r'v([0-9\.]*-?[0-9]*)?'
+COMMAND_DESCRIBE = ['git', 'describe', '--tags', '--exact-match']
+COMMAND_DESCRIBE_VERSION = ['git', 'describe', '--tags']
+COMMAND_REV_PARSE = ['git', 'rev-parse', '--short', 'HEAD']
+SUBPROCESS_KWARGS = {
+    'cwd': script_path,
+    'stdout': subprocess.PIPE,
+    'stderr': subprocess.PIPE
+}
 
 
 class PythonVersionException(Exception):
@@ -14,25 +28,28 @@ class PythonVersionException(Exception):
         return repr(self.message)
 
 
+def get_setup_version():
+    """Return a version string that can be used with the setup method. Includes
+    additional commits since the last tagged commit.
+    """
+    process = subprocess.Popen(COMMAND_DESCRIBE_VERSION, **SUBPROCESS_KWARGS)
+    process.wait()
+    version = process.communicate()[0].decode("utf-8").strip()
+    return re.match(re_version, version).group(1)
+
+
 def get_version():
     """Returns a version that will either result as "0.1" style string if the
     git commit is tagged or as "git-ebe0338" otherwise. If there is no
     directory named ".git'", the function will instead return False.
     """
-    COMMAND_DESCRIBE = ['git', 'describe', '--tags', '--exact-match']
-    COMMAND_REV_PARSE = ['git', 'rev-parse', '--short', 'HEAD']
-    KWARGS = {
-        'cwd': script_path,
-        'stdout': subprocess.PIPE,
-        'stderr': subprocess.PIPE
-    }
     version_is_commit = False
     if os.path.isdir('.git'):
-        process = subprocess.Popen(COMMAND_DESCRIBE, **KWARGS)
+        process = subprocess.Popen(COMMAND_DESCRIBE, **SUBPROCESS_KWARGS)
         process.wait()
         if process.returncode != 0:
             version_is_commit = True
-            process = subprocess.Popen(COMMAND_REV_PARSE, **KWARGS)
+            process = subprocess.Popen(COMMAND_REV_PARSE, **SUBPROCESS_KWARGS)
             process.wait()
         version = process.communicate()[0].decode("utf-8").strip()
         if version_is_commit:
@@ -45,6 +62,10 @@ def get_version():
 
 
 def write_version_file():
+    """Writes a version.py file containing the current version. Only replaces
+    the first line of the file so that the rest of the variables and functions
+    can still be used normally.
+    """
     version_file_path = os.path.join(script_path, 'cum/version.py')
     with open(version_file_path, 'r') as file:
         lines = file.readlines()
@@ -58,18 +79,13 @@ if sys.version_info[0] < 3:
     raise PythonVersionException('Python 3.x required.')
 
 
-script_path = os.path.dirname(os.path.realpath(__file__))
-git_path = os.path.join(script_path, '.git')
-
 version = get_version()
 if version:
     write_version_file()
 
-from cum.version import __version__
-
 setup(
     name='cum',
-    version=__version__,
+    version=get_setup_version(),
     description='comic updater, mangafied',
     url='https://github.com/Hamuko/cum',
     license='Apache2',
