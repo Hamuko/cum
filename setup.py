29caaf781715d567c7cd73cebceb7f406a2d1fb4
Hamuko <hamuko@burakku.com>
2016-04-28 18:53:36 +0300

    Add additional information to setup.py for PyPI.

--- setup.py
+++ setup.py
@@ -88,7 +88,20 @@ setup(
     version=get_setup_version(),
     description='comic updater, mangafied',
     url='https://github.com/Hamuko/cum',
+    author='Hamuko',
+    author_email='hamuko@burakku.com',
     license='Apache2',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Internet :: WWW/HTTP'
+    ],
     packages=find_packages(),
     install_requires=[
         'alembic',
